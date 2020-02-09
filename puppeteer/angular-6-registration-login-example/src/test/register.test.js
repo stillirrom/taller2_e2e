33593561e24dbacbb0fa8937abf6cf3517e8252c
Daury <daurylecca16@gmail.com>
2020-02-08 21:04:36 -0500

    Test registro

--- puppeteer/angular-6-registration-login-example/src/test/register.test.js
+++ puppeteer/angular-6-registration-login-example/src/test/register.test.js
@@ -1,35 +1,78 @@
 const timeout = process.env.SLOWMO ? 30000 : 10000;
 
-beforeAll(async () => {
-    await page.goto(URL, {waitUntil: 'domcontentloaded'});
-});
+beforeAll(async () => { 
+    await page.goto(URL+'/register', {waitUntil: 'domcontentloaded'}) 
+    page.setViewport({ width: 500, height: 2400 })
+})
 
-describe('Test header and title of the page', () => {
-    test('Title of the page', async () => {
-        const title = await page.title();
-        expect(title).toBe('Angular 6 User Registration and Login Example');
+describe('Test Registro', () => {
+    test('Pagina Registro', async () => {
+        await page.waitForSelector('h2');
+        const html = await page.$eval('h2', el => el.textContent);
+
+        expect(html).toEqual('Register');
     }, timeout);
-
-    /*test('Header of the page', async () => {
-        const headerHandle = await page.$('.learn_header');
-        const html = await page.evaluate(headerHandle => headerHandle.innerHTML, headerHandle);
-
-        expect(html).toBe("What will you learn");
-    }, timeout);*/
-
-    /*test('Submit form with valid data', async () => {
-        await page.click('[href="/register"]');
+    test('Formulario vacío', async () => {
+        await page.click('button[class="btn btn-primary"]');
+        const listItems = await page.$$('.invalid-feedback')
+        // Se debe recibir cuatro errores debido a que existen 4 inputs
+        expect(listItems.length).toBe(4)
+    }, timeout);
+    test('Formulario vacío 2', async () => {
+        await page.click('button[class="btn btn-primary"]');
+        const html = await page.$eval('.invalid-feedback', el => el.textContent);
         
+        expect(html).toEqual('First Name is required');
+    }, timeout);
+    test('Formulario solo con FirstName', async () => {
+        await page.type('input[formControlName="firstName"]', 'Daury',{delay: 10});
+        await page.click('button[class="btn btn-primary"]');
+        const listItems = await page.$$('.invalid-feedback')
+        
+        expect(listItems.length).toBe(3)
+    }, timeout);
+    test('Formulario con FirstName y LastName', async () => {
+        await page.type('input[formControlName="firstName"]', 'Daury',{delay: 10});
+        await page.type('input[formControlName="lastName"]','Lecca',{delay: 10});
+        await page.click('button[class="btn btn-primary"]');
+        const listItems = await page.$$('.invalid-feedback')
+        
+        expect(listItems.length).toBe(2)
+    }, timeout);
+    test('Formulario con FirstName y LastName', async () => {
+        await page.type('input[formControlName="firstName"]', 'Daury',{delay: 10});
+        await page.type('input[formControlName="lastName"]','Lecca',{delay: 10});
+        await page.type('input[formControlName="username"]','dn.lecca', {delay: 10});
+        await page.click('button[class="btn btn-primary"]');
+        
+        const html = await page.$eval('.invalid-feedback', el => el.textContent);
+        const listItems = await page.$$('.invalid-feedback')
+        
+        expect(listItems.length).toBe(1)
+        expect(html).toEqual('Password is required');
+    }, timeout);
+    test('Password con menos de 6 caracteres', async () => {
         await page.waitForSelector('form');
-        await page.type('#name', 'Rick');
+        await page.type('input[formControlName="firstName"]', 'Daury',{delay: 10});
+        await page.type('input[formControlName="lastName"]','Lecca',{delay: 10});
+        await page.type('input[formControlName="username"]','dn.lecca', {delay: 10});
+        await page.type('input[formControlName="password"]','123', {delay: 10});
+        await page.click('button[class="btn btn-primary"]');
+        const html = await page.$eval('.invalid-feedback', el => el.textContent);
 
-        await page.type('#password','szechuanSauce');
-        await page.type('#repeat_password','szechuanSauce');
+        expect(html).toEqual('Password must be at least 6 characters');
+    }, timeout);
+    
+    test('Registro exitoso', async () => {
+        await page.waitForSelector('form');
+        await page.type('input[formControlName="firstName"]', 'Daury',{delay: 10});
+        await page.type('input[formControlName="lastName"]','Lecca',{delay: 10});
+        await page.type('input[formControlName="username"]','dn.lecca', {delay: 10});
+        await page.type('input[formControlName="password"]','1234567890', {delay: 10});
+        await page.click('button[class="btn btn-primary"]');
+        await page.waitForSelector('.alert-success');
+        const html = await page.$eval('.alert-success', el => el.textContent);
 
-        await page.click('[type="submit"]');
-        await page.waitForSelector('.success');
-        const html = await page.$eval('.success', el => el.innerHTML);
-
-        expect(html).toBe('Successfully signed up!');
-    }, timeout);*/
+        expect(html).toEqual('Registration successful');
+    }, timeout);
 });
